443628d92b7315cb31942865d41a478675fcd54d
Hao_CHEN <applications.chen.hao@gmail.com>
2024-02-20 19:57:35 +0100

    Merge blocklog_csv_process.py & utils/data_loader.py -> utils/data_processing.py

--- utils/data_processing.py
+++ utils/data_processing.py
@@ -1,5 +1,15 @@
 from pathlib import Path
 import pandas as pd
+import re
+
+
+def check_ip_address(df, column):
+    # Regular expression for matching IPv4 & IPv6 addresses with non-capturing groups
+    ipv4_pattern = r'(?:\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
+    ipv6_pattern = r'(?:[0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}(?:\/\d{1,3})?'
+    ip_pattern = f'(?:{ipv4_pattern}|{ipv6_pattern})'
+
+    return df[column].str.contains(ip_pattern, regex=True)
 
 
 def file_iterator(dir_path, start_year, end_year, extract_cols):
