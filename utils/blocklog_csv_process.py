0404f9d3525a238f00e73543581bfcda77b08b15
Hao_CHEN <applications.chen.hao@gmail.com>
2024-02-20 13:05:00 +0100

    Calcualte administrator monthly metrics

--- utils/blocklog_csv_process.py
+++ utils/blocklog_csv_process.py
@@ -1,17 +1,6 @@
 import pandas as pd
 import re
 
-def clean_df(df):
-    df = df.drop_duplicates()
-    # remove rows containing the column names
-    df = df[df['title']!='title']
-    # remove rows containing missing information on user/administrator names
-    df = df[~df['title'].isna() & ~df['user'].isna()]
-    # remove 'User:' string from the title column
-    df['title'] = df['title'].str[5:]
-    # remove rows where the administrator block and unblock themselves
-    df = df[df['user'] != df['title']]
-    return df
 
 def check_ip_address(df, column):
     # Regular expression for matching IPv4 & IPv6 addresses with non-capturing groups
